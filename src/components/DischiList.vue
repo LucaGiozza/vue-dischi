306f6856c7cfe0c9e6500fd721b6b2b089eb31ac
Luca Giozza <luca.giozza@gmail.com>
2021-07-08 15:03:37 +0200

    init e header

---
+++ src/components/DischiList.vue
@@ -0,0 +1,22 @@
+<template>
+<section>
+    <div class="row">
+        <div class="col-5"></div>
+        prova
+
+    </div>
+
+</section>
+  
+</template>
+
+<script>
+export default {
+    name: 'DischiList'
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,29 @@
+<template>
+<header>
+    <img class="logo" src="@/assets/spotify.jpg" alt="">
+</header>
+
+  
+</template>
+
+<script>
+export default {
+    name : 'Header'
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+header{
+    background-color: black;
+    width:100%;
+
+    .logo{
+    width:80px;
+}
+
+}
+
+
+</style>
